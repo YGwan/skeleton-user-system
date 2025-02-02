18a3ed4b66156408ad62eae1631f610d301f6661
Yonggwan Park <ygpark@conportlab.com>
2025-02-02 16:37:31 +0900

    feat: server env

---
+++ src/test/java/com/skeletonusersystem/SkeletonUserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeletonusersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonUserSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
