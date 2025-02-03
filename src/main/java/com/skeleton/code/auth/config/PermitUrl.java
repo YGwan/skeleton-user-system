54a70a2686728bc3d1b09f944c3ca211673162a3
YGwan <good0619@naver.com>
2025-02-04 00:33:01 +0900

    feat : define permit url class (permit_urls & content_urls)

---
+++ src/main/java/com/skeleton/code/auth/config/PermitUrl.java
@@ -0,0 +1,14 @@
+package com.skeleton.code.auth.config;
+
+public class PermitUrl {
+
+    public static final String[] PERMIT_URLS = new String[]{
+    };
+
+    public static final String[] CONTENT_URLS = new String[]{
+        "/v3/api-docs/**",
+        "/swagger-ui/**",
+        "/swagger-ui.html",
+        "/h2-console/**"
+    };
+}
